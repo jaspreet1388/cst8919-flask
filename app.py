b29ee5a2cfce8bc1ec3a33e53b3ad3ffac61e03f
jaspreet1388 <chha0038@algonquinlive.com>
2025-07-06 21:58:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,13 @@ def patched_save_session(self, app, session, response):
     session_id = session.sid
     if isinstance(session_id, bytes):
         session_id = session_id.decode('utf-8')
-    response.set_cookie(app.session_cookie_name, session_id, **self.get_cookie_params(app))
+    response.set_cookie(
+        app.session_cookie_name,
+        session_id,
+        httponly=True,
+        samesite=app.config.get("SESSION_COOKIE_SAMESITE", "Lax"),
+        secure=app.config.get("SESSION_COOKIE_SECURE", False)
+    )
     return original_save_session(self, app, session, response)
 
 FileSystemSessionInterface.save_session = patched_save_session
